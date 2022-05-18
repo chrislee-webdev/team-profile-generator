d66e48ac3f87882e0408ce99ad2677184c90a39a
Christopher <leechrs1@gmail.com>
2022-05-17 19:12:35 -0500

    constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,4 +6,8 @@ test('create an employee object', () => {
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String))
+
+    getName();
+
+    getId();
 })
