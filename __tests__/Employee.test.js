9acd542012e1c50a11440938c7bf2d3ce12e850a
Christopher <leechrs1@gmail.com>
2022-05-17 19:29:03 -0500

    engineer test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,11 +3,14 @@ const Employee = require('../lib/Employee.js');
 test('create an employee object', () => {
     const employee = new Employee('Bob', 1, 'bob@email.com');
 
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String))
+    expect(employee.name).toBe("Bob")
+    expect(employee.id).toBe(1);
+    expect(employee.email).toBe("bob@email.com")
 
-    getName();
-
-    getId();
+    expect(employee.getName()).toBe("Bob")
+    expect(employee.getId()).toBe(1);
+    expect(employee.getEmail()).toBe("bob@email.com")
+    expect(employee.getRole())
+    .toBe("Employee")
+   
 })
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,20 @@
 const Engineer = require('../lib/Engineer.js');
 
-test('create and engineer object', () => {
-    const engineer = new Engineer();
-})
+const engineer = new Engineer('Bob', 1, 'bob@email.com','GitHubRepo');
+
+    test('create an engineer object', () => {
+        
+    
+        expect(engineer.name).toBe("Bob")
+        expect(engineer.id).toBe(1);
+        expect(engineer.email).toBe("bob@email.com")
+        expect(engineer.github).toBe("GitHubRepo")
+    
+        expect(engineer.getName()).toBe("Bob")
+        expect(engineer.getId()).toBe(1);
+        expect(engineer.getEmail()).toBe("bob@email.com")
+        expect(engineer.getRole())
+        .toBe("Engineer")
+        expect(engineer.getGithub()).toBe("GitHubRepo")
+       
+    })
